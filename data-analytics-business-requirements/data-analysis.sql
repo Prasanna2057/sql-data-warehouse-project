7fa8d4b4355911504241050f1b333d2d178fd5c2
Prasanna2057 <128151006+Prasanna2057@users.noreply.github.com>
2026-09-03 17:09:37 +0545

    Create data-analysis.sql

---
+++ data-analytics-business-requirements/data-analysis.sql
@@ -0,0 +1,228 @@
+
+-- Change-Over-Time(Trends)
+SELECT
+    trunc(order_date, 'YYYY')    AS order_date,
+    SUM(sales_amount)            AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity)                AS total_quantity
+FROM
+    gold.fact_sales
+WHERE
+    order_date IS NOT NULL
+GROUP BY
+    trunc(order_date, 'YYYY')
+ORDER BY
+    trunc(order_date, 'YYYY');
+
+
+-- Calculate the total sales per month
+-- and the running total of sales and moving average price over time (Cumulative Analysis)
+ SELECT
+    order_date,
+    total_sales,
+    SUM(total_sales)
+    OVER(
+        ORDER BY
+            order_date
+    )        AS running_total_sales,
+    round(AVG(avg_price)
+          OVER(
+        ORDER BY
+            order_date
+          ),
+          2) AS moving_average_price
+FROM
+    (
+        SELECT
+            trunc(order_date, 'MM') AS order_date,
+            SUM(sales_amount)       AS total_sales,
+            AVG(price)              AS avg_price
+        FROM
+            gold.fact_sales
+        WHERE
+            order_date IS NOT NULL
+        GROUP BY
+            trunc(order_date, 'MM')
+    ) t
+
+   
+    /* Analyze the yearly performance of products by comparing their sales
+    to both the average sales performance of the product and the previous year's sales */
+WITH yearly_product_sales AS (
+    SELECT
+        EXTRACT(YEAR FROM f.order_date) AS order_year,
+        p.product_name,
+        SUM(f.sales_amount)             AS current_sales
+    FROM
+        gold.fact_sales   f
+        LEFT JOIN gold.dim_products p ON p.product_key = f.product_key
+    WHERE
+        f.order_date IS NOT NULL
+    GROUP BY
+        EXTRACT(YEAR FROM f.order_date),
+        p.product_name
+)
+SELECT
+    order_year,
+    product_name,
+    current_sales,
+    round(AVG(current_sales)
+          OVER(PARTITION BY product_name),
+          2) AS avg_sales,
+    current_sales - round(AVG(current_sales)
+                          OVER(PARTITION BY product_name),
+                          2)     AS diff_avg,
+    CASE
+        WHEN current_sales - round(AVG(current_sales)
+                                   OVER(PARTITION BY product_name),
+                                   2) > 0 THEN
+            'Above Average'
+        WHEN current_sales - round(AVG(current_sales)
+                                   OVER(PARTITION BY product_name),
+                                   2) < 0 THEN
+            'Below Average'
+        ELSE
+            'Average'
+    END      AS avg_change,
+--Year over year Analysis
+    LAG(current_sales)
+    OVER(PARTITION BY product_name
+         ORDER BY
+             order_year
+    )        AS py_sales,
+    current_sales - LAG(current_sales)
+                    OVER(PARTITION BY product_name
+                         ORDER BY
+                             order_year
+    )        AS diff_py,
+    CASE
+        WHEN current_sales - LAG(current_sales)
+                             OVER(PARTITION BY product_name
+                                  ORDER BY
+                                      order_year
+        ) > 0 THEN
+            'Increase'
+        WHEN current_sales - LAG(current_sales)
+                             OVER(PARTITION BY product_name
+                                  ORDER BY
+                                      order_year
+        ) < 0 THEN
+            'Decrease'
+        ELSE
+            'No Change'
+    END      AS py_change
+FROM
+    yearly_product_sales;
+
+
+    --Which categories contribute the most to overall sales?
+WITH category_sales AS (
+    SELECT
+        category,
+        SUM(sales_amount) AS total_sales
+    FROM
+        gold.fact_sales f
+        LEFT JOIN dim_products    p ON p.product_key = f.product_key
+    GROUP BY
+        category
+)
+SELECT
+    category,
+    total_sales,
+    SUM(total_sales)
+    OVER() AS overall_sales,
+    concat(
+        round((total_sales / SUM(total_sales)
+                             OVER()) * 100,
+              2),
+        '%'
+    )      AS percentage_of_total
+FROM
+    category_sales
+ORDER BY
+    total_sales DESC;
+
+
+/* Segment products into cost ranges and
+count how many products fall into each segment */
+WITH product_segments AS (
+    SELECT
+        product_key,
+        product_name,
+        cost,
+        CASE
+            WHEN cost < 100                THEN
+                'Below 100'
+            WHEN cost BETWEEN 100 AND 500  THEN
+                '100-500'
+            WHEN cost BETWEEN 500 AND 1000 THEN
+                '500-1000'
+            ELSE
+                'Above 1000'
+        END cost_range
+    FROM
+        gold.dim_products
+)
+SELECT
+    cost_range,
+    COUNT(product_key) AS total_products
+FROM
+    product_segments
+GROUP BY
+    cost_range
+ORDER BY
+    total_products DESC;
+
+
+/* Group customers into three segments based on their spending behavior:
+    -VIP: Customers with at least 12 months of history and spending more than 5,000
+    -Regular: Customers with at least 12 months of history but spending 5000 or less
+    -New: Customer with a lifespan less than 12 months.
+And find the total number of customers by each group
+*/
+WITH customer_spending AS (
+    SELECT
+        c.customer_key,
+        SUM(f.sales_amount) AS total_spending,
+        MIN(order_date)     AS first_order,
+        MAX(order_date)     AS last_order,
+        trunc(months_between(
+            max(order_date),
+            min(order_date)
+        ))                  AS lifespan
+    FROM
+        gold.fact_sales    f
+        LEFT JOIN gold.dim_customers c ON c.customer_key = f.customer_key
+    GROUP BY
+        c.customer_key
+)
+SELECT
+    customer_segment,
+    COUNT(customer_key) AS total_customers
+FROM
+    (
+        SELECT
+            customer_key,
+            CASE
+                WHEN total_spending > 5000
+                     AND lifespan >= 12 THEN
+                    'VIP'
+                WHEN total_spending <= 5000
+                     AND lifespan >= 12 THEN
+                    'Regular'
+                ELSE
+                    'New'
+            END AS customer_segment
+        FROM
+            customer_spending
+    )
+GROUP BY
+    customer_segment
+ORDER BY
+    total_customers DESC;
+
+
+
+    
+    
+    
